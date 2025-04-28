31d79fd12e7d70b2e72dcb9c9835df6b859f20de
gaau511 <gaau511@gmail.com>
2025-04-28 16:05:33 +0900

    feat: Create Member entity

---
+++ src/main/java/dev/gaau/login/domain/Member.java
@@ -0,0 +1,47 @@
+package dev.gaau.login.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "member")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "birth", nullable = false)
+    private Date birth;
+
+    @CreationTimestamp
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime createdAt;
+}
