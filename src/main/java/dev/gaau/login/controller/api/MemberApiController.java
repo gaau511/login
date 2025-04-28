d36310145f98cf82df9e1d33a67aff0d149598bb
gaau511 <gaau511@gmail.com>
2025-04-28 16:07:50 +0900

    feat: Implement member signup method

---
+++ src/main/java/dev/gaau/login/controller/api/MemberApiController.java
@@ -0,0 +1,26 @@
+package dev.gaau.login.controller.api;
+
+import dev.gaau.login.dto.request.SignUpRequestDto;
+import dev.gaau.login.dto.response.MemberResponseDto;
+import dev.gaau.login.serivce.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<MemberResponseDto> signUp(@RequestBody SignUpRequestDto request) {
+        MemberResponseDto memberDto = memberService.join(request);
+
+        return ResponseEntity.ok(memberDto);
+    }
+}
