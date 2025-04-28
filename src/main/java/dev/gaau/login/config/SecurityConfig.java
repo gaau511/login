d46e9c0f08bdb61d2b2b039f2731a06276cd1111
gaau511 <gaau511@gmail.com>
2025-04-28 16:22:55 +0900

    feat: Implement Security configuration by SecurityFilterChain
    - Disable csrf.
    - Resources accessible without authentication :
    	+ "/" : welcome page
    	+ "/error" : default error page
    	+ "/*/member/signUp" : sign up page
    	+ "/*member/login" : login page
    	+ swagger relate urls

---
+++ src/main/java/dev/gaau/login/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package dev.gaau.login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                    authorizeRequests
+                            .requestMatchers("/", "/error").permitAll()
+                            .requestMatchers("/*/member/signUp").permitAll()
+                            .requestMatchers("/*/member/login").permitAll()
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                            .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
