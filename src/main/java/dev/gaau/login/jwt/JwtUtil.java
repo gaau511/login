931b4050c9aec05acfb3f34998143c4251ebcbee
gaau511 <gaau511@gmail.com>
2025-04-29 22:57:03 +0900

    feat: Implement a method to issue jwt tokens
    - Add methods to issue access token and refresh token to JwtUtil class.
    - The access token has a validity of 10 minutes and the refresh token
      has a validity of 30 days
    - Sign the token using the HMAC SHA256 algorithm
    - Set userId to subject for user identification
    - Included claims :
    	- iss
    	- aud
    	- exp
    	- sub

---
+++ src/main/java/dev/gaau/login/jwt/JwtUtil.java
@@ -0,0 +1,63 @@
+package dev.gaau.login.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.time.Instant;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final String issuer;
+    private final Long accessTokenExpTime;
+    private final Long refreshTokenExpTime;
+
+    public JwtUtil(@Value("${jwt.secret-key}") String key,
+                   @Value("${jwt.issuer}") String issuer,
+                   @Value("${jwt.expiration-time.access}") Long accessTokenExpTime,
+                   @Value("${jwt.expiration-time.refresh}") Long refreshTokenExpTime) {
+        byte[] decodedKey = Decoders.BASE64.decode(key);
+        this.secretKey = Keys.hmacShaKeyFor(decodedKey);
+        this.issuer = issuer;
+        this.accessTokenExpTime = accessTokenExpTime;
+        this.refreshTokenExpTime = refreshTokenExpTime;
+    }
+
+    public String createAccessToken(Long memberId, String audience) {
+
+        String accessToken = Jwts.builder()
+                .subject(String.valueOf(memberId))
+                .issuer(issuer)
+                .audience().add(audience).and()
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plusMillis(accessTokenExpTime)))
+
+                .signWith(secretKey)
+                .compact();
+
+        return accessToken;
+    }
+
+    public String createRefreshToken(Long memberId, String audience) {
+
+        String refreshToken = Jwts.builder()
+                .subject(String.valueOf(memberId))
+                .issuer(issuer)
+                .audience().add(audience).and()
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plusMillis(refreshTokenExpTime)))
+
+                .signWith(secretKey)
+                .compact();
+
+        return refreshToken;
+    }
+
+}
