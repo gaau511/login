a25371f3c4a12a298dabada9f9f4e4f97e18f653
gaau511 <gaau511@gmail.com>
2025-04-30 19:25:27 +0900

    feat: Update password storage policy (raw -> encrypt)
    - Update to store encrypted passwords for security.
    - Using bcrypt hash algorithm.

--- src/main/java/dev/gaau/login/serivce/MemberService.java
+++ src/main/java/dev/gaau/login/serivce/MemberService.java
@@ -1,11 +1,18 @@
 package dev.gaau.login.serivce;
 
 import dev.gaau.login.domain.Member;
+import dev.gaau.login.dto.request.LoginRequestDto;
 import dev.gaau.login.dto.request.SignUpRequestDto;
+import dev.gaau.login.dto.response.LoginResponseDto;
 import dev.gaau.login.dto.response.MemberResponseDto;
+import dev.gaau.login.jwt.JwtUtil;
 import dev.gaau.login.mapper.MemberMapper;
 import dev.gaau.login.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +23,17 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public MemberResponseDto join(SignUpRequestDto request) {
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        request.setPassword(encodedPassword);
+
         Member member = memberMapper.signUpRequestDtoToMember(request);
         Member savedMember = memberRepository.save(member);
 
         return memberMapper.memberToMemberResponseDto(savedMember);
     }
+
 }
