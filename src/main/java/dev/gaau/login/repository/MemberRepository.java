adc243b3b1b174dd2fc31a8811549f97bf3f5ced
gaau511 <gaau511@gmail.com>
2025-04-30 19:31:07 +0900

    feat: Implement findByUsername method for member

--- src/main/java/dev/gaau/login/repository/MemberRepository.java
+++ src/main/java/dev/gaau/login/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package dev.gaau.login.repository;
 import dev.gaau.login.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
