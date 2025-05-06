23865d31bfce43d45551d0c38c3e959518795d93
gaau511 <gaau511@gmail.com>
2025-05-06 17:23:11 +0900

    feat: Add CustomAuthenticationFilter in SecurityFilterChain

--- src/main/java/dev/gaau/login/config/SecurityConfig.java
+++ src/main/java/dev/gaau/login/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package dev.gaau.login.config;
 
+import dev.gaau.login.filter.CustomAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,7 +29,9 @@ public class SecurityConfig {
                             .requestMatchers("/*/member/login").permitAll()
                             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                             .anyRequest().authenticated()
-        );
+
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
